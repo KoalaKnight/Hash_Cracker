54ea381d0982a236d95a54e842dd7a3fbd41beae
Toby Flynn <toby_flynn@btinternet.com>
2018-10-20 20:32:13 +0100

    Added class description

--- HashFunction.java
+++ HashFunction.java
@@ -1,3 +1,8 @@
+/**
+* HashFunction is an interface for classes that provide hashing algorithms to implement.
+* 
+* @author       Toby Flynn
+*/
 public interface HashFunction {
     
     public String hashPassword(String password);
