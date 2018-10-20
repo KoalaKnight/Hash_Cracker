204518cc0ad4df73b0bfc2878bea3cee8786275b
Toby Flynn <toby_flynn@btinternet.com>
2018-10-20 20:30:09 +0100

    Added comments

--- MD5Hash.java
+++ MD5Hash.java
@@ -2,12 +2,23 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+/**
+* MD5Hash class implements HashFunction and contains a method to hash a string using the MD5 algorithm.
+* 
+* @author           Toby Flynn
+*/
 public class MD5Hash implements HashFunction {
     
     public MD5Hash() {
         
     }
     
+    /**
+    * Method that hashes a given string using the MD5 algorithm.
+    * 
+    * @param        password        The string to be hashed
+    * @return                       The hash as a string
+    */
     public String hashPassword(String password) {
         String hash = null;
         try {
